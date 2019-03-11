88af339821393112da8049326c11c6318bfd98bc
seeduardo <romenflatiron@gmail.com>
2019-03-11 13:51:49 +0000

    Lab completed

--- js/app/directives/couponCode.js
+++ js/app/directives/couponCode.js
@@ -1,5 +1,14 @@
 function couponCode() {
 
+	return {
+		require: 'ngModel',
+		link: (s, e, a, n) => {
+			n.$validators.correct = input => {
+				return /\d{2}[a-z]{4}\d{2}/i.test(input)
+			}
+		}
+	}
+
 }
 
 angular
